3c7eec82772a3dce8a9e945da68322bf8134f761
Ana Paula Teodoro Carvalho <d2024009328@unifei.edu.br>
2024-11-05 09:14:25 -0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "ESP32-Anape";
+const char* password = "12344321";
+
+WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/html", "<html><body><h1>ESP da Anape</h1><p>Teste do ESP32 na aula de ECOP11A</p></body></html>");
+  Serial.println("Pagina Enviada ao Cliente");
+}
+
+void handleControl(){
+  if (server.hasArg("action")) {
+    String action = server.arg("action");
+    if (action == "acionar") {
+      Serial.println("Botão Acionar pressionado");
+      digitalWrite(26, !digitalRead(26));
+    }
+  }
+  server.send(200, "text/html", "<html><body><h1>Controle</h1><form action=\"/control\" method=\"POST\"><button type=\"submit\" name=\"action\" value=\"acionar\">Acionar</button></form></body></html>");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  WiFi.softAP(ssid, password,2);
+
+  pinMode(26, OUTPUT);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/", handleRoot);
+  server.on("/control", handleControl);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  server.handleClient();
 }
